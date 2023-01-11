e2405e1336829ddb08f881dacf3c0735c03f4cf9
mikiri <mikiri@mkr.local>
2023-01-11 16:58:39 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -12,10 +12,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <RouterProvider router={router}/>
     </React.StrictMode>)
 
-
-export default function main() {
-    return (<Router>
-        <LogIn>
-        </LogIn>
-    </Router>)
-}
